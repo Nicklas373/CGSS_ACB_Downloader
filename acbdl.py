04013400c369e1178cecbb95d8cba137358d370d
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2019-01-15 05:51:31 +0700

    Fix derp with directory and update dependencies for latest python 3 windows
    - Derp directory named scheme ".//", for now all md5 hash file will downloaded and placed on existing directory, all bgm and sound files will mixed into one directory (Will try to moved it after learn some python language :v)
    - Update dependencies for latest python (3.7) on windows

--- acbdl.py
+++ acbdl.py
@@ -1,4 +1,5 @@
 #!py -3
+#!/usr/bin/env python
 
 def usage():
 	print("\tUsage:run.py [-v <VERSION>] [-g] [-s] [-m]")
@@ -58,15 +59,15 @@ if not version:
 
 if verbose:
 	print("\tGame Version = "+version)
-if not os.path.exists(".\\"+version):
+if not os.path.exists(""+version):
 	os.mkdir(version)
 
 dl_headers={'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 7.0; FRD-AL00 Build/HUAWEIFRD-AL00)','X-Unity-Version': '5.4.5p1','Accept-Encoding': 'gzip'}
 
-if not os.path.exists(".\\manifests"):
-	os.mkdir(".\\manifests")
-dbname=".\\manifests\\manifest_"+version+".db"
-lz4name=".\\manifests\\manifest_"+version+".db.lz4"
+if not os.path.exists("manifests"):
+	os.mkdir("manifests")
+dbname="manifestsmanifest_"+version+".db"
+lz4name="manifestsmanifest_"+version+".db.lz4"
 if not os.path.exists(dbname):
 	if not os.path.exists(lz4name):
 		if verbose:
@@ -100,7 +101,7 @@ db=sqlite3.Connection(dbname)
 if gennamelist:
 	if verbose:
 		print("\tGenerating namelist.csv ...")
-	namelist=open(version+"\\list_name_hash_sha1.csv",'w')
+	namelist=open(version+"list_name_hash_sha1.csv",'w')
 	query=db.execute("select name,hash from manifests")
 	namelist.write("name,md5,sha1\n")
 	for i in query:
@@ -112,19 +113,19 @@ if gennamelist:
 	query.close()
 	namelist.close()
 query=db.execute("select name,hash from manifests where name like 'b/%.acb'")
-fp1=open(version+"\\b_ren1.bat",'w')
-fp2=open(version+"\\b_ren2.bat",'w')
+fp1=open(version+"b_ren1.bat",'w')
+fp2=open(version+"b_ren2.bat",'w')
 for name,hash in query:
 	fp1.write("ren "+hash+' '+name[2:]+'\n')
 	fp2.write("ren "+name[2:]+' '+hash+'\n')
-	if not os.path.exists(".\\b\\"+hash):
+	if not os.path.exists("\\b\\"+hash):
 		if verbose:
 			print("\tDownloading file "+hash+'('+name+')')
 		url="http://storage.game.starlight-stage.jp/dl/resources/High/Sound/Common/b/"+hash
-		dlfilefrmurl(url,".\\b\\"+hash,dl_headers)
+		dlfilefrmurl(url,"b"+hash,dl_headers)
 	else:
 		if md5chk:
-			with open(".\\b\\"+hash,'rb') as fp:
+			with open("b"+hash,'rb') as fp:
 				buf=fp.read()
 				fp.close()
 				md5res=hashlib.md5(buf).hexdigest()
@@ -133,7 +134,7 @@ for name,hash in query:
 				if verbose:
 					print("\tFile "+hash+'('+name+')'+" didn't pass md5check, delete and re-downloading ...")
 				url="http://storage.game.starlight-stage.jp/dl/resources/High/Sound/Common/b/"+hash
-				dlfilefrmurl(url,".\\b\\"+hash,dl_headers)
+				dlfilefrmurl(url,"b"+hash,dl_headers)
 			elif verbose:
 				print("\tFile "+hash+'('+name+')'+" already exists")
 		elif verbose:
@@ -141,19 +142,19 @@ for name,hash in query:
 fp1.close()
 fp2.close()
 query=db.execute("select name,hash from manifests where name like 'l/%.acb'")
-fp1=open(version+"\\l_ren1.bat",'w')
-fp2=open(version+"\\l_ren2.bat",'w')
+fp1=open(version+"l_ren1.bat",'w')
+fp2=open(version+"l_ren2.bat",'w')
 for name,hash in query:
 	fp1.write("ren "+hash+' '+name[2:]+'\n')
 	fp2.write("ren "+name[2:]+' '+hash+'\n')
-	if not os.path.exists(".\\l\\"+hash):
+	if not os.path.exists("l"+hash):
 		if verbose:
 			print("\tDownloading file "+hash+'('+name+')')
 		url="http://storage.game.starlight-stage.jp/dl/resources/High/Sound/Common/l/"+hash
-		dlfilefrmurl(url,".\\l\\"+hash,dl_headers)
+		dlfilefrmurl(url,"l"+hash,dl_headers)
 	else:
 		if md5chk:
-			with open(".\\l\\"+hash,'rb') as fp:
+			with open("l"+hash,'rb') as fp:
 				buf=fp.read()
 				fp.close()
 				md5res=hashlib.md5(buf).hexdigest()
@@ -162,7 +163,7 @@ for name,hash in query:
 				if verbose:
 					print("\tFile "+hash+'('+name+')'+" didn't pass md5check, delete and re-downloading ...")
 				url="http://storage.game.starlight-stage.jp/dl/resources/High/Sound/Common/l/"+hash
-				dlfilefrmurl(url,".\\l\\"+hash,dl_headers)
+				dlfilefrmurl(url,".l"+hash,dl_headers)
 			elif verbose:
 				print("\tFile "+hash+'('+name+')'+" already exists")
 		elif verbose:
