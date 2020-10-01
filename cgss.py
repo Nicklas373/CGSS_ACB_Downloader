42a71c0135d963e3b572b1739ee5ed7b41b65203
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2020-10-02 05:55:08 +0700

    cgss: Update latest solo voice song
    Song 1011 (Orange sapphire)

--- cgss.py
+++ cgss.py
@@ -494,11 +494,40 @@ for name,hash in query:
 				print("\tFile "+hash+'('+name+')'+" already exists")
 		elif verbose:
 			print("\tFile "+hash+'('+name+')'+" already exists")
+			query=db.execute("select name,hash from manifests where name like 'l/song_9008_part/%.awb'")
+part_7=version+"/solo/song_1011_part"
+if os.path.isdir(version+"\\solo\\song_1011_part\\"):
+    print("")
+else:
+    os.makedirs(part_7)
+fp1=open(version+"\\solo\\song_1011_part\\p_ren1.bat",'w')
+fp2=open(version+"\\solo\\song_1011_part\\p_ren2.bat",'w')
+for name,hash in query:
+	fp1.write("ren "+hash+' '+name[2:]+'\n')
+	fp2.write("ren "+name[2:]+' '+hash+'\n')
+	if not os.path.exists(version+"\\solo\\song_1011_part\\"+hash):
+		if verbose:
+			print("\tDownloading file "+hash+'('+name+')')
+		url="http://asset-starlight-stage.akamaized.net/dl/resources/Sound/"+hash[:2]+"/"+hash
+		dlfilefrmurl(url,version+"\\solo\\song_1011_part\\"+hash,dl_headers)
+	else:
+		if md5chk:
+			with open("\\solo\\song_1011_part\\"+hash,'rb') as fp:
+				buf=fp.read()
+				fp.close()
+				md5res=hashlib.md5(buf).hexdigest()
+				del(buf)
+			if md5res!=hash:
+				if verbose:
+					print("\tFile "+hash+'('+name+')'+" didn't pass md5check, delete and re-downloading ...")
+				url="http://asset-starlight-stage.akamaized.net/dl/resources/Sound/"+hash[:2]+"/"+hash
+				dlfilefrmurl(url,version+"\\solo\\song_1011_part\\"+hash,dl_headers)
+			elif verbose:
+				print("\tFile "+hash+'('+name+')'+" already exists")
+		elif verbose:
+			print("\tFile "+hash+'('+name+')'+" already exists")
 fp1.close()
 fp2.close()
-db.close()
-if verbose:
-	print("\tFinished analysing database ...")
 
 print("\tCopying python file for next process ...")
 shutil.copy("bak.py", version)
