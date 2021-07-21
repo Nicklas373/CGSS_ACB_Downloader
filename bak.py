56197bcd7eef3dd492fcbcbfeb07452af22be50b
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2021-07-21 09:10:32 +0700

    bak: Use copytree argument for shutil
    Use shutil.copytree for better argument on copy or moved all file/folder in folder source to folder destination on latest python version (3.8+)
    
    " shutil.copytree(src, dest, dirs_exist_ok=True)  # 3.8+ only! "
    
    Also fixup issue when backup solo_acb that include separate solo voice over songs
    
    References:
    - https://stackoverflow.com/questions/12683834/how-to-copy-directory-recursively-in-python-and-overwrite-all

--- bak.py
+++ bak.py
@@ -8,17 +8,18 @@ import shutil
 path_bgm = "bgm/"
 path_sound = "sound/"
 path_solo = "solo/"
+path_se = "se/"
 path_bgm_moved = "bgm_acb"
 path_sound_moved = "sound_acb"
 path_solo_moved = "solo_acb"
+path_se_moved = "se_acb"
 
 # define move all files to specific directory command
 files = os.listdir(path_bgm)
 
 os.mkdir(path_bgm_moved)
 try:
-    for f in files:
-        shutil.copy(path_bgm+f, path_bgm_moved)
+    shutil.copytree(path_bgm, path_bgm_moved, dirs_exist_ok=True)
 except OSError:
     print ("Copy files from %s to static directory failed" % path_bgm_moved)
 else:
@@ -29,8 +30,7 @@ files = os.listdir(path_sound)
 
 os.mkdir(path_sound_moved)
 try:
-    for f in files:
-        shutil.copy(path_sound+f, path_sound_moved)
+    shutil.copytree(path_sound, path_sound_moved, dirs_exist_ok=True)
 except OSError:
     print ("Copy files from %s to static directory failed" % path_sound_moved)
 else:
@@ -41,11 +41,21 @@ files = os.listdir(path_solo)
 
 os.mkdir(path_solo_moved)
 try:
-    for f in files:
-        shutil.copy(path_solo+f, path_solo_moved)
+    shutil.copytree(path_solo, path_solo_moved, dirs_exist_ok=True)
 except OSError:
     print ("Copy files from %s to static directory failed" % path_solo_moved)
 else:
      print ("Moving files from %s to static directory success" % path_solo_moved)
+
+# define move all files to specific directory command
+files = os.listdir(path_se)
+
+os.mkdir(path_se_moved)
+try:
+    shutil.copytree(path_se, path_se_moved, dirs_exist_ok=True)
+except OSError:
+    print ("Copy files from %s to static directory failed" % path_se_moved)
+else:
+     print ("Moving files from %s to static directory success" % path_se_moved)
      
 print ("Script done, finally :p")
