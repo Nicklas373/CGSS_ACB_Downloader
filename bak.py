194f66637294a1468703af7649c3207a54634893
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2021-09-02 14:27:15 +0700

    bak: Add function to remove dummy files

--- bak.py
+++ bak.py
@@ -25,5 +25,23 @@ while i < 4:
     else:
         print ("Moving files from %s to static directory success" % path_moved[i])
     i += 1
-     
+    
+# remove unused or unindexed files from original directory
+i = 0
+while i < 4:
+    for root, _, files in os.walk(cgss_win_path + cgss_manifest_ver + "/" + path_orig[i]):
+        for f in files:
+            fullpath = os.path.join(root, f)
+            try:
+                if os.path.getsize(fullpath) < 7 * 1024:   #set file size in kb
+                    print (fullpath)
+                    if fullpath.endswith('.bat'):
+                        print (fullpath + " was excluded due a bat files")
+                    else:
+                        print (fullpath + " was removed, size below than 7kb")
+                        os.remove(fullpath)
+            except WindowsError:
+                print ("Error" + fullpath)
+    i += 1
+            
 print ("Script done, finally :p")
