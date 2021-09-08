44e030c0e27a7820bece2984d0a48fc6825e35f4
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2021-09-08 13:23:59 +0700

    Update cgss.py

--- cgss.py
+++ cgss.py
@@ -179,7 +179,7 @@ while i < 3:
         print("\tDownloading assets for: "+song_in_folder[i]+"...")
         query=db.execute("select name,hash,size from manifests where name like '"+song_in_alias[i]+"/%.acb'")
         cgss_folder=version+"/"+song_in_folder[i]
-        if os.path.isdir(version+"\\"+song_in_folder[i]+"\\"):
+        if path.exists(version+"\\"+song_in_folder[i]+"\\"):
             print("")
         else:
             os.makedirs(cgss_folder)
@@ -226,8 +226,8 @@ for song_in_query in song_part_list:
         print("\tDownloading assets for: "+song_in_query+"...")
         query=db.execute("select name,hash,size from manifests where name like 'l/"+song_in_query+"/%.awb'")
         part=version+"/solo/"+song_in_query
-        if os.path.isdir(version+"\\solo\\"):
-                if os.path.isdir(version+"\\solo\\"+song_in_query+"\\"):
+        if path.exists(version+"\\solo\\"):
+                if path.exists(version+"\\solo\\"+song_in_query+"\\"):
                         print("")
                 else:
                         os.makedirs(part)
