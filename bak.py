39e7de1446bf099bbb0c50d5b135ee6373278e8c
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2021-11-28 06:58:07 +0700

    bak: Fixup backup function after simplified
    Make sure this script run on top op project folder or same location where cgss.py located

--- bak.py
+++ bak.py
@@ -1,5 +1,7 @@
 # import the os module
 import os, os.path
+from os import path
+from pathlib import Path
 
 # import the shutil module (move directory)
 import shutil
@@ -8,21 +10,22 @@ import shutil
 import numpy as np
 
 # define the name of the directory to be created
-cgss_path = os.getcwd()
+cgss_win_path = os.getcwd()
 path_orig = np.array(["bgm/", "sound/", "solo/", "se/"])
 path_moved = np.array(["bgm_acb", "sound_acb", "solo_acb", "se_acb"])
-f=Path(cgss_win_path+"\\Static_version")
+f=Path("Static_version")
 f=open(f, 'r')
 cgss_manifest_ver=f.read()
 f.close()
+path_backup = cgss_win_path+"//"+cgss_manifest_ver+"//"
 
 # define move all files to specific directory command
 i = 0
 while i < 4:
-    files = os.listdir(path_orig[i])
-    os.mkdir(path_moved[i])
+    files = os.listdir(path_backup+path_orig[i])
+    os.mkdir(path_backup+path_moved[i])
     try:
-        shutil.copytree(path_orig[i], path_moved[i], dirs_exist_ok=True)
+        shutil.copytree(path_backup+path_orig[i], path_backup+path_moved[i], dirs_exist_ok=True)
     except OSError:
         print("\tCopy files from %s to static directory failed" % path_moved[i])
     else:
@@ -32,7 +35,7 @@ while i < 4:
 # remove unused or unindexed files from original directory
 i = 0
 while i < 4:
-    for root, _, files in os.walk(cgss_win_path+"\\"+cgss_manifest_ver+"\\"+path_moved[i]):
+    for root, _, files in os.walk(path_backup+path_moved[i]):
         for f in files:
             fullpath = os.path.join(root, f)
             try:
@@ -47,4 +50,3 @@ while i < 4:
     i += 1
             
 print("\tScript done, finally :p")
-sys.exit()
