52d64807441898fa81ccee12cb671f15f6c573cc
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-09-27 21:46:24 +0700

    rename: misc change
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- rename.py
+++ rename.py
@@ -10,6 +10,8 @@ from os import path
 from pathlib import Path
 import sqlite3,hashlib
 import numpy as np
+import csv
+from csv import writer, reader
 
 version=None
 
@@ -21,6 +23,16 @@ if not version:
         print("\tCurrent manifest version = "+version)
         print("\tPlease run cgss.py if you want to get latest manifest version!")
 
+def implementNewColandRow(inputcsv,outputcsv,newheader,newrow):
+        reader = csv.reader(open(inputcsv, 'r', encoding='UTF8', newline=''))
+        writer = csv.writer(open(outputcsv, 'w', encoding='UTF8', newline=''))
+        headers = next(reader)
+        headers.append(newheader)
+        writer.writerow(headers)
+        for row in reader:
+            row.append(newrow)
+            writer.writerow(row)
+            
 cgss_path=os.getcwd()
 cgss_logs=cgss_path+"\\logs"
 manifests=".\\manifests\\manifest_"+version+".db"
@@ -96,6 +108,11 @@ for solo_in_query in solo_list:
                 os.remove(version+"\\solo\\"+solo_in_query+"\\sva_ren2.bat")
         else:
                 print("")
+
+if path.exists(cgss_logs+"\\rename"):
+    print("")
+else:
+    os.makedirs(cgss_logs+"\\rename\\")
         
 print("\tBegin generate music name list from database...")
 print("\tCreate name list for "+version+"/sound in database from manifests ...\n")
@@ -107,7 +124,7 @@ for name,hash,size in query:
     else:
             print("")
     f.close()
-
+    
 song_main_name_list = np.loadtxt(cgss_logs+"\\rename\\"+"song_main_name_list.txt", dtype=str, delimiter=",")
 for song_main_name in song_main_name_list:
     query=music_data_con.execute("select id, name from music_data where id like '"+song_main_name+"'")
@@ -120,168 +137,20 @@ for song_main_name in song_main_name_list:
         fp2.write("ren "+str(replace_phase_2).replace('/n','')+".hca"+' '+"song_"+str(id)+".hca"+"\n")
         fp1.close()
         fp2.close()
-
-print("\tCreate auto rename for "+version+"/sound_another in database from manifests ...\n")
-query=manifests_con.execute("select name,hash,size from manifests where name like 'l/song_%_another.acb' and size > '7000' and name not like 'l/song_%_call.acb' and name not like 'l/song_%_collab.acb' and name not like 'l/song_%_se.acb' and name not like 'l/song_%_movie.acb'")
-for name,hash,size in query:
-    f=open(cgss_logs+"\\rename\\song_another_name_list.txt", 'a')
-    f.write(name[7:][:-4]+"\n")
-    f.close()
-
-song_another_name_list = np.loadtxt(cgss_logs+"\\rename\\"+"song_another_name_list.txt", dtype=str, delimiter=",")
-for song_another_name in song_another_name_list:
-    query=music_data_another_con.execute("select music_id, name from music_another_data where music_id like '"+song_another_name+"'")
-    for music_id,name in query:
-        fp1=open(version+"\\sound\\s_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\sound\\s_ren2.bat", 'a', encoding='utf8')
-        fp1.write("ren song_"+music_id+".hca"+ ' '+str(name).replace(' ','_')+"_another.hca"+"\n")
-        fp2.write("ren "+str(name).replace(' ','_')+"_another.hca"+' '+"song_"+music_id+".hca"+"\n")
-        fp1.close()
-        fp2.close()
-
-print("\tCreate auto rename for "+version+"/sound_call in database from manifests ...\n")
-query=manifests_con.execute("select name,hash,size from manifests where name like 'l/song_%_call.acb' and size > '7000' and name not like 'l/song_%_another.acb' and name not like 'l/song_%_collab.acb' and name not like 'l/song_%_se.acb' and name not like 'l/song_%_movie.acb'")
-for name,hash,size in query:
-    f=open(cgss_logs+"\\rename\\song_call_name_list.txt", 'a')
-    f.write(name[7:][:-4]+"\n")
-    f.close()
-
-song_call_name_list = np.loadtxt(cgss_logs+"\\rename\\"+"song_call_name_list.txt", dtype=str, delimiter=",")
-for song_call_name in song_call_name_list:
-    query=music_data_another_con.execute("select music_id, name from music_another_data where music_id like '"+song_call_name+"'")
-    for music_id,name in query:
-        fp1=open(version+"\\sound\\s_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\sound\\s_ren2.bat", 'a', encoding='utf8')
-        fp1.write("ren song_"+music_id+".hca"+ ' '+str(name).replace(' ','_')+"_call.hca"+"\n")
-        fp2.write("ren "+str(name).replace(' ','_')+"_call.hca"+' '+"song_"+music_id+".hca"+"\n")
-        fp1.close()
-        fp2.close()
-
-query=manifests_con.execute("select name,hash,size from manifests where name like 'l/song_%_collab.acb' and size > '7000' and name not like 'l/song_%_another.acb' and name not like 'l/song_%_call.acb' and name not like 'l/song_%_se.acb' and name not like 'l/song_%_movie.acb'")
-print("\tCreate auto rename for "+version+"/sound_collab in database from manifests ...\n")
-for name,hash,size in query:
-    f=open(cgss_logs+"\\rename\\song_collab_name_list.txt", 'a')
-    f.write(name[7:][:-4]+"\n")
-    f.close()
-
-song_collab_name_read = open(cgss_logs+"\\rename\\"+"song_collab_name_list.txt", "r")
-song_collab_name = song_collab_name_read.read()
-query=music_data_con.execute("select id, name from music_data where id like '"+song_collab_name[:-8]+"'")
-for id,name in query:
-        fp1=open(version+"\\sound\\s_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\sound\\s_ren2.bat", 'a', encoding='utf8')
-        fp1.write("ren song_"+str(id)+".hca"+ ' '+str(name).replace(' ','_')+"_collab.hca"+"\n")
-        fp2.write("ren "+str(name).replace(' ','_')+"_collab.hca"+' '+"song_"+str(id)+".hca"+"\n")
-        fp1.close()
-        fp2.close()
-
-query=manifests_con.execute("select name,hash,size from manifests where name like 'l/song_%_se.acb' and size > '7000' and name not like 'l/song_%_another.acb' and name not like 'l/song_%_collab.acb' and name not like 'l/song_%_call.acb' and name not like 'l/song_%_movie.acb'")
-print("\tCreate auto rename for "+version+"/sound_se in database from manifests ...\n")
-for name,hash,size in query:
-    f=open(cgss_logs+"\\rename\\song_se_name_list.txt", 'a')
-    f.write(name[7:][:-4]+"\n")
-    f.close()
-
-song_se_name_list = np.loadtxt(cgss_logs+"\\rename\\"+"song_se_name_list.txt", dtype=str, delimiter=",")
-for song_se_name in song_se_name_list:
-    query=music_data_con.execute("select id, name from music_data where id like '"+song_se_name[:-3]+"'")
-    for id,name in query:
-        fp1=open(version+"\\sound\\s_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\sound\\s_ren2.bat", 'a', encoding='utf8')
-        fp1.write("ren song_"+str(id)+".hca"+ ' '+str(name).replace(' ','_')+"_se.hca"+"\n")
-        fp2.write("ren "+str(name).replace(' ','_')+"_se.hca"+' '+"song_"+str(id)+".hca"+"\n")
-        fp1.close()
-        fp2.close()
-
-query=manifests_con.execute("select name,hash,size from manifests where name like 'l/song_%_movie.acb' and size > '7000' and name not like 'l/song_%_another.acb' and name not like 'l/song_%_collab.acb' and name not like 'l/song_%_call.acb' and name not like 'l/song_%_se.acb'")
-print("\tCreate auto rename for "+version+"/sound_movie in database from manifests ...\n")
-for name,hash,size in query:
-    f=open(cgss_logs+"\\rename\\song_movie_name_list.txt", 'a')
-    f.write(name[7:][:-4]+"\n")
-    f.close()
-
-song_movie_name_read = open(cgss_logs+"\\rename\\"+"song_movie_name_list.txt", "r")
-song_movie_name_list = song_movie_name_read.read()
-query=music_data_con.execute("select id, name from music_data where id like '"+song_movie_name_list[:-7]+"'")
-for id,name in query:
-        fp1=open(version+"\\sound\\s_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\sound\\s_ren2.bat", 'a', encoding='utf8')
-        fp1.write("ren song_"+str(id)+".hca"+ ' '+str(name).replace(' ','_')+"_movie.hca"+"\n")
-        fp2.write("ren "+str(name).replace(' ','_')+"_movie.hca"+' '+"song_"+str(id)+".hca"+"\n")
-        fp1.close()
-        fp2.close()
-        
-print("\tCreate auto rename for "+version+"/sound_1001 in database from manifests ...\n")
-query=manifests_con.execute("select name,hash,size from manifests where name like 'l/song_1001_%.acb' and size > '7000' and name not like 'l/song_%_call.acb' and name not like 'l/song_%_collab.acb' and name not like 'l/song_%_se.acb' and name not like 'l/song_%_movie.acb'")
-for name,hash,size in query:
-    f=open(cgss_logs+"\\rename\\song_1001_vocal_list.txt", 'a')
-    f.write(name[12:][:-4]+"\n")
-    f.close()
-
-song_1001_vocal_list = np.loadtxt(cgss_logs+"\\rename\\"+"song_1001_vocal_list.txt", dtype=str, delimiter=",")
-for song_1001_vocal in song_1001_vocal_list:
-    query=vocal_data_con.execute("select chara_id, name from chara_data where chara_id like '"+song_1001_vocal+"'")
-    for chara_id,name in query:
-        fp1=open(version+"\\sound\\s_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\sound\\s_ren2.bat", 'a', encoding='utf8')
-        fp1.write("ren song_1001_"+str(chara_id)+".hca"+ ' お願い_!_シンデレラ~'+str(name).replace(' ','_')+".hca"+"\n")
-        fp2.write("ren お願い_!_シンデレラ~"+str(name).replace(' ','_')+".hca"+' '+"song_1001_"+str(chara_id)+".hca"+"\n")
-        fp1.close()
-        fp2.close()
-
-print("\tCreate auto rename for "+version+"/sound_solo_part in database from manifests ...\n")
-solo_list = np.loadtxt(cgss_logs+"\\"+"solo_list.txt", dtype=str, delimiter=",")
-for solo_in_query in solo_list:
-        query = manifests_con.execute("select name, hash, size from manifests where name like 'l/"+solo_in_query+"/vo_solo_%_0%.awb'")
-        f=open(cgss_logs+"\\rename\\"+solo_in_query+"_vocal.txt", 'a')
-        for name, hash, size in query:
-                f.write(str(name[31:][:-4])+"\n")
-        f.close()
-
-solo_list = np.loadtxt(cgss_logs+"\\"+"solo_list.txt", dtype=str, delimiter=",")
-for solo_in_query in solo_list:
-        sub_solo_list = np.loadtxt(cgss_logs+"\\rename\\"+solo_in_query+"_vocal.txt", dtype=str, delimiter=",")
-        fp1=open(version+"\\solo\\"+solo_in_query+"\\sva_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\solo\\"+solo_in_query+"\\sva_ren2.bat", 'a', encoding='utf8')
-        for sub_solo_list_in_query in sub_solo_list:
-                solo_id_trim = solo_in_query[5:][:-5]
-                query1 = music_data_con.execute("select id, name as music_name from music_data where id like '"+solo_id_trim+"'")
-                for id, music_name in query1:
-                        if (sub_solo_list_in_query[3:] == "_1"):
-                                trim_sub_solo = sub_solo_list_in_query[:-2]
-                        elif (sub_solo_list_in_query[3:] == "_2"):
-                                trim_sub_solo = sub_solo_list_in_query[:-2]
-                        else:
-                                trim_sub_solo = sub_solo_list_in_query
-                        query2 = vocal_data_con.execute("select chara_id, name as chara_name from chara_data where chara_id like '"+trim_sub_solo+"'")
-                        for chara_id, chara_name in query2:
-                                solo_id_trim_2 = solo_in_query[5:][:-5]
-                                if (sub_solo_list_in_query[3:] == "_1"):
-                                        trim_sub_solo_2 = str(chara_id)+"_1"
-                                        trim_sub_chara_2 = str(chara_name)+"_1"
-                                elif (sub_solo_list_in_query[3:] == "_2"):
-                                        trim_sub_solo_2 = str(chara_id)+"_2"
-                                        trim_sub_chara_2 = str(chara_name)+"_2"
-                                else:
-                                        trim_sub_solo_2 = str(chara_id)
-                                        trim_sub_chara_2 = str(chara_name)
-                                fp1.write("ren vo_solo_"+solo_id_trim_2+"_0"+str(trim_sub_solo_2)+".awb"+' '+str(music_name.replace('\n',"_"))+"~"+str(trim_sub_chara_2)+".awb"+"\n")
-                                fp2.write("ren "+str(music_name.replace('\n',"_"))+"~"+str(trim_sub_chara_2)+".awb"+' '+"vo_solo_"+solo_id_trim_2+"_0"+str(trim_sub_solo_2)+".awb"+"\n")
-                                fp1.close
-                                fp2.close           
-
-
-solo_list = np.loadtxt(cgss_logs+"\\"+"solo_list.txt", dtype=str, delimiter=",")
-for solo_in_query in solo_list:
-        fp1=open(version+"\\solo\\"+solo_in_query+"\\sva_ren1.bat", 'a', encoding='utf8')
-        fp2=open(version+"\\solo\\"+solo_in_query+"\\sva_ren2.bat", 'a', encoding='utf8')
-        solo_id_trim = solo_in_query[5:][:-5]
-        query1 = music_data_con.execute("select id, name as music_name from music_data where id like '"+solo_id_trim+"'")
-        for id, music_name in query1:
-                solo_id_trim_2 = solo_in_query[5:][:-5]
-                fp1.write("ren inst_song_"+solo_id_trim_2+".awb"+' '+"inst_"+str(music_name).replace(" ","_")+".awb"+"\n")
-                fp2.write("ren inst_"+str(music_name).replace(" ","_")+".awb"+' '+"inst_song_"+solo_id_trim_2+".awb"+"\n")
-        fp1.close
-        fp2.close
-      
+        default_text = str(name)
+        # Open the input_file in read mode and output_file in write mode
+        with open(cgss_logs+'\\sound.csv', 'r', encoding='UTF8') as read_obj, \
+                open('output_1.csv', 'w', newline='', encoding='UTF8') as write_obj:
+            # Create a csv.reader object from the input file object
+            csv_reader = reader(read_obj)
+            # Create a csv.writer object from the output file object
+            csv_writer = writer(write_obj)
+            # Read each row of the input csv file as list
+            for row in csv_reader:
+                    for id,name in query:
+                        # Append the default text in the row / list
+                        row.append(str(name))
+                        # Add the updated row / list to the output file
+                        csv_writer.writerow(row)
+            
 print("\tCGSS ACB Renamer List | Finished!")      
